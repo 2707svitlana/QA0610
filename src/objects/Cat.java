d71d7396a25d3bc1379d7b30ba96b72fc0687f1b
админ <2707svitlana@gmail.com>
2022-11-06 23:55:13 +0300

    HomeWork6

--- src/objects/Cat.java
+++ src/objects/Cat.java
@@ -7,14 +7,14 @@ public class Cat extends Animal {
         this.color = color;
     }
     public Cat(boolean vegetarian, String eats, int noOfLegs){
-        super();
+        super(vegetarian, eats,noOfLegs);
         this.color = "white";
-    }
-    public String getColor() {
-       return color;
     }
     public void setColor(String color) {
         this.color = color;
+    }
+    public String getColor() {
+       return color;
     }
     public void print(){
     System.out.println (getColor() +" "+"cat"+" "+"eats"+" "+getEats()+" "+"has"+" "+getNoOfLegs()+"legs");
